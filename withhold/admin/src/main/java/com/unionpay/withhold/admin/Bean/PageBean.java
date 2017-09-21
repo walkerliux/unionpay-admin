71a1df51786177a178ac30d888c351e9a9b09f9e
lianhai <lianhai@192.168.11.1>
2017-09-21 11:41:22 +0800

    按easyui格式要求，修改分页查询的PageBean

--- withhold/admin/src/main/java/com/unionpay/withhold/admin/Bean/PageBean.java
+++ withhold/admin/src/main/java/com/unionpay/withhold/admin/Bean/PageBean.java
@@ -3,19 +3,24 @@ package com.unionpay.withhold.admin.Bean;
 public class PageBean {
 	private int total;
 	private Object rows;
+
 	public int getTotal() {
 		return total;
 	}
+
 	public void setTotal(int total) {
 		this.total = total;
 	}
-	public Object getData() {
+
+	public Object getRows() {
 		return rows;
 	}
-	public void setData(Object rows) {
+
+	public void setRows(Object rows) {
 		this.rows = rows;
 	}
-	public PageBean(int total, Object rows){
+
+	public PageBean(int total, Object rows) {
 		this.total = total;
 		this.rows = rows;
 	}
