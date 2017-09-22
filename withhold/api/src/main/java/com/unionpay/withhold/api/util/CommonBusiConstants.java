c28b65e6ac3c7938e50549945068ffc40885e135
bema <bema@DESKTOP-NTQ29PF>
2017-09-22 16:32:18 +0800

    实时代收,实时查询接口

---
+++ withhold/api/src/main/java/com/unionpay/withhold/api/util/CommonBusiConstants.java
@@ -0,0 +1,28 @@
+package com.unionpay.withhold.api.util;
+
+/**
+ * 
+ * @author Luke
+ *
+ */
+public class CommonBusiConstants {
+
+	/**
+	 * 服务报文版本号
+	 */
+	public static final String TRANS_VERSION = "1.0.0";
+	/**
+	 * 服务报文编码
+	 */
+	public static final String TRANS_ENCODING = "UTF-8";
+
+	/**
+	 * 服务报文加密函数
+	 */
+	public static final String TRANS_SIGNMETHOD = "RSA";
+	/**
+	 * 服务币种
+	 */
+	public static final String TRANS_ATTYPE = "156";
+
+}
