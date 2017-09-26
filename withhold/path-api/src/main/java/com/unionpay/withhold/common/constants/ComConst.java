a60b83c5fd8eb8a39303c60a53b85a8556e1eeaf
maxinwei <maxinwei@sh-paytong.com>
2017-09-26 09:50:25 +0800

    test架构

---
+++ withhold/path-api/src/main/java/com/unionpay/withhold/common/constants/ComConst.java
@@ -0,0 +1,30 @@
+package com.unionpay.withhold.common.constants;
+
+public class ComConst {
+	/**
+	 * S-成功GATEWAY
+	 */
+	public static final String SUCCESS = "S";
+	/**
+	 * W-警告
+	 */
+	public static final String WARNING = "W";
+	/**
+	 * E-错误
+	 */
+	public static final String ERROR = "E";
+	/**
+	 * U-未知
+	 */
+	public static final String UNKNOWN = "U";
+	/**
+	 * 成功返回码
+	 */
+	public static final String SUC_RET_COD = "S00001";
+	/**
+	 * 成功返回信息
+	 */
+	public static final String SUC_RET_MSG = "SUCCESS";
+
+	
+}
---
+++ withhold/path-cp/src/main/java/com/unionpay/withhold/path/app/impl/RealtimeWithholdQryImpl.java
@@ -0,0 +1,26 @@
+package com.unionpay.withhold.path.app.impl;
+
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.unionpay.withhold.common.bean.ResultBean;
+import com.unionpay.withhold.path.services.RealtimeWithholdQrySrv;
+import com.unionpay.withhold.service.path.cp.app.RealtimeWithholdQry;
+
+@Service("realtimeWithholdQry")
+public class RealtimeWithholdQryImpl implements RealtimeWithholdQry {
+
+    private final static Logger logger = LoggerFactory.getLogger(RealtimeWithholdQryImpl.class);
+    @Resource
+    private RealtimeWithholdQrySrv realtimeWithholdQrySrv;
+
+    @Override
+    public ResultBean qryRTWthdRecord(String txnseqno) {
+        logger.info("================【txnseqno】：" + txnseqno);
+        return realtimeWithholdQrySrv.qryRTWthdRecord(txnseqno);
+    }
+
+}
