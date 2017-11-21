6af582bfc14691f5a02e1004214d6248f8fe9672
sssplus <425631294@qq.com>
2017-11-21 09:27:41 +0800

    日期转换工具类修改

--- withhold/admin/src/main/java/com/unionpay/withhold/admin/utils/DateTimeReplaceUtil.java
+++ withhold/admin/src/main/java/com/unionpay/withhold/admin/utils/DateTimeReplaceUtil.java
@@ -1,5 +1,9 @@
 package com.unionpay.withhold.admin.utils;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 
 public class DateTimeReplaceUtil {
@@ -11,9 +15,22 @@ public class DateTimeReplaceUtil {
 		String time3 = time2.replace(" ", "");
 		return time3;
 	} 
+	//将时间字符串转化为date
+	public static Date simpleformat(String time) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date parse=null;
+		try {
+			parse = sdf.parse(time);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return parse;
+	}
 	public static void main(String[] args) {
 		String etime="2017-03-01 16:50:48";
-		String time = DateTimeReplaceUtil.replace(etime);
-		System.out.println(time);
+		//String time = DateTimeReplaceUtil.replace(etime);
+		Date date = DateTimeReplaceUtil.simpleformat(etime);
+		System.out.println(date);
 	}
 }
