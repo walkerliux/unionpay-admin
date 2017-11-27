4f489ed483500599bb20c8895d2162cd55bfefbd
lianhai <lianhai@192.168.11.1>
2017-11-27 15:08:55 +0800

    添加“t_merch_checkfile”的pojo和mapper

---
+++ withhold/admin/src/main/java/com/unionpay/withhold/admin/pojo/TMerchCheckfile.java
@@ -0,0 +1,83 @@
+package com.unionpay.withhold.admin.pojo;
+
+public class TMerchCheckfile {
+    private Integer tid;
+
+    private String merchno;
+
+    private String checkdate;
+
+    private String intime;
+
+    private String filename;
+
+    private String uri;
+
+    private String requesttime;
+
+    private String url;
+
+    public Integer getTid() {
+        return tid;
+    }
+
+    public void setTid(Integer tid) {
+        this.tid = tid;
+    }
+
+    public String getMerchno() {
+        return merchno;
+    }
+
+    public void setMerchno(String merchno) {
+        this.merchno = merchno == null ? null : merchno.trim();
+    }
+
+    public String getCheckdate() {
+        return checkdate;
+    }
+
+    public void setCheckdate(String checkdate) {
+        this.checkdate = checkdate == null ? null : checkdate.trim();
+    }
+
+    public String getIntime() {
+        return intime;
+    }
+
+    public void setIntime(String intime) {
+        this.intime = intime == null ? null : intime.trim();
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename == null ? null : filename.trim();
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri == null ? null : uri.trim();
+    }
+
+    public String getRequesttime() {
+        return requesttime;
+    }
+
+    public void setRequesttime(String requesttime) {
+        this.requesttime = requesttime == null ? null : requesttime.trim();
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url == null ? null : url.trim();
+    }
+}
